483804e651e5236fab56e62435feccafd44a7803
thaibao-15 <phamthaibao2005thd@gmail.com>
2025-11-07 15:34:18 +0700

    add View view

--- app/src/main/java/com/example/thiennguyen/view/TrangChu/HomeFragment.java
+++ app/src/main/java/com/example/thiennguyen/view/TrangChu/HomeFragment.java
@@ -12,11 +12,15 @@ import com.example.thiennguyen.R;
 
 
 public class HomeFragment extends Fragment {
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        
+        return view;
     }
 }
