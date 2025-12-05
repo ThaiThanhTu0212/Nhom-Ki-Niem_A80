0028443b052d385ed5ae33de32e8180b69f73e02
thaibao-15 <phamthaibao2005thd@gmail.com>
2025-12-05 22:16:53 +0700

    add verify email

--- app/src/main/java/com/example/thiennguyen/view/taikhoan/LoginActivity.java
+++ app/src/main/java/com/example/thiennguyen/view/taikhoan/LoginActivity.java
@@ -22,6 +22,8 @@ import com.example.thiennguyen.view.data.DTO.request.AuthenticationRequest;
 import com.example.thiennguyen.view.data.api.AuthenticationApi;
 import com.example.thiennguyen.view.data.sharepreference.DataLocalManager;
 import com.example.thiennguyen.view.login.RegisterActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -32,6 +34,7 @@ public class LoginActivity extends AppCompatActivity {
     TextView txtForgotPassLogin;
     Button btnLogin, btnDangKy;
     AuthenticationApi authenticationApi = ApiClient.getRetrofit().create(AuthenticationApi.class);
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,36 +46,56 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        mAuth = FirebaseAuth.getInstance();
         initUI();
         initListener();
     }
+
     public void checkLogin(String email, String password) {
-        AuthenticationRequest authenticationRequest = new AuthenticationRequest(email, password);
-        Call<ApiResponse<AuthenticationResponse>> callLogin = authenticationApi.login(authenticationRequest);
 
-        callLogin.enqueue(new Callback<ApiResponse<AuthenticationResponse>>() {
-            @Override
-            public void onResponse(Call<ApiResponse<AuthenticationResponse>> call, Response<ApiResponse<AuthenticationResponse>> response) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        if (user != null && user.isEmailVerified()) {
+                            AuthenticationRequest authenticationRequest = new AuthenticationRequest(email, password);
+                            Call<ApiResponse<AuthenticationResponse>> callLogin = authenticationApi.login(authenticationRequest);
 
-                if (response.isSuccessful() && response.body() != null) {
-                    // Đăng nhập thành công
-                    DataLocalManager.setToken(response.body().getResult().getToken());
-                    Toast.makeText(LoginActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finishAffinity();
-                }else {
-                    // ❌ Sai email hoặc mật khẩu
-                    Toast.makeText(LoginActivity.this, "Email hoặc mật khẩu không đúng!", Toast.LENGTH_SHORT).show();
-                }
-            }
+                            callLogin.enqueue(new Callback<ApiResponse<AuthenticationResponse>>() {
+                                @Override
+                                public void onResponse(Call<ApiResponse<AuthenticationResponse>> call, Response<ApiResponse<AuthenticationResponse>> response) {
 
-            @Override
-            public void onFailure(Call<ApiResponse<AuthenticationResponse>> call, Throwable t) {
-                // ❌ Lỗi mạng hoặc lỗi server
-                Toast.makeText(LoginActivity.this, "Lỗi kết nối. Vui lòng thử lại!", Toast.LENGTH_SHORT).show();
-            }
-        });
+                                    if (response.isSuccessful() && response.body() != null) {
+                                        // Đăng nhập thành công
+                                        DataLocalManager.setToken(response.body().getResult().getToken());
+                                        Toast.makeText(LoginActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                        finishAffinity();
+                                    }else {
+                                        // ❌ Sai email hoặc mật khẩu
+                                        Toast.makeText(LoginActivity.this, "Email hoặc mật khẩu không đúng!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+
+                                @Override
+                                public void onFailure(Call<ApiResponse<AuthenticationResponse>> call, Throwable t) {
+                                    // ❌ Lỗi mạng hoặc lỗi server
+                                    Toast.makeText(LoginActivity.this, "Lỗi kết nối. Vui lòng thử lại!", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Vui lòng xác thực email trước!", Toast.LENGTH_LONG).show();
+                            mAuth.signOut();
+                        }
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Đăng nhập thất bại, email hoặc mật khẩu không đúng! " ,
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+
+
+
     }
 
 
@@ -80,8 +103,13 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(v -> {
             String email = edtEmailLogin.getText().toString().trim();
             String password = edtPasswordLogin.getText().toString().trim();
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Vui lòng nhập đầy đủ thông tin!", Toast.LENGTH_SHORT).show();
+                return;
+            }
 
             checkLogin(email, password);
+
 
         });
         btnDangKy.setOnClickListener(v -> {
