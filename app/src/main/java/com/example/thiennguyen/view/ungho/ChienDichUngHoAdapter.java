151fe2159011dec88af565275e0609fcbca959f7
TuanAnhNguyen-2005 <Tn271574@gmail.com>
2025-12-05 16:04:01 +0700

    quay lai

--- app/src/main/java/com/example/thiennguyen/view/ungho/ChienDichUngHoAdapter.java
+++ app/src/main/java/com/example/thiennguyen/view/ungho/ChienDichUngHoAdapter.java
@@ -4,7 +4,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,18 +13,13 @@ import com.bumptech.glide.Glide;
 import com.example.thiennguyen.R;
 import com.example.thiennguyen.view.model.ChienDich;
 
-import java.math.RoundingMode;
-import java.text.NumberFormat;
 import java.util.List;
-import java.util.Locale;
 
 public class ChienDichUngHoAdapter extends RecyclerView.Adapter<ChienDichUngHoAdapter.ChienDichUngHoViewHolder> {
-    private final List<ChienDich> chienDichList;
-    private final OnChienDichClickListener onChienDichClickListener;
+    List<ChienDich> chienDichList;
 
-    public ChienDichUngHoAdapter(List<ChienDich> chienDichList, OnChienDichClickListener onChienDichClickListener) {
+    public ChienDichUngHoAdapter(List<ChienDich> chienDichList) {
         this.chienDichList = chienDichList;
-        this.onChienDichClickListener = onChienDichClickListener;
     }
 
     @NonNull
@@ -33,58 +27,36 @@ public class ChienDichUngHoAdapter extends RecyclerView.Adapter<ChienDichUngHoAd
     public ChienDichUngHoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_chiendich_ungho, parent, false);
+
         return new ChienDichUngHoViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ChienDichUngHoViewHolder holder, int position) {
         ChienDich chienDich = chienDichList.get(position);
-        holder.bind(chienDich, onChienDichClickListener);
+
+        holder.tvTenChienDichUngHo.setText(chienDich.getTenCd());
+        holder.tvSotienHienTai.setText(chienDich.getSoTienHienTai().toString()+" VND");
+        holder.tvSotienMucTieu.setText(chienDich.getSoTienMucTieu().toString()+" VND");
+        Glide.with(holder.itemView.getContext())
+                .load(chienDich.getHinhAnh())
+                .into(holder.imgchiendichUngHo);
     }
 
     @Override
     public int getItemCount() {
-        return chienDichList != null ? chienDichList.size() : 0;
+        return chienDichList!=null? chienDichList.size():0;
     }
 
-    public interface OnChienDichClickListener {
-        void onChienDichClick(ChienDich chienDich);
-    }
-
-    static class ChienDichUngHoViewHolder extends RecyclerView.ViewHolder {
+    public class ChienDichUngHoViewHolder extends RecyclerView.ViewHolder {
         ImageView imgchiendichUngHo;
-        TextView tvTenChienDichUngHo, tvSotienHienTai, tvSotienMucTieu;
-        ProgressBar progressBarItem;
-
+        TextView tvTenChienDichUngHo,tvSotienHienTai,tvSotienMucTieu;
         public ChienDichUngHoViewHolder(@NonNull View itemView) {
             super(itemView);
             imgchiendichUngHo = itemView.findViewById(R.id.imgchiendichUngHo);
             tvTenChienDichUngHo = itemView.findViewById(R.id.tvTenChienDichUngHo);
             tvSotienHienTai = itemView.findViewById(R.id.tvSotienHienTai);
             tvSotienMucTieu = itemView.findViewById(R.id.tvSotienMucTieu);
-            progressBarItem = itemView.findViewById(R.id.progressBarItem);
-        }
-
-        void bind(final ChienDich chienDich, final OnChienDichClickListener listener) {
-            tvTenChienDichUngHo.setText(chienDich.getTenCd());
-
-            NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(new Locale("vi", "VN"));
-            tvSotienHienTai.setText(currencyFormat.format(chienDich.getSoTienHienTai()));
-            tvSotienMucTieu.setText("trên " + currencyFormat.format(chienDich.getSoTienMucTieu()));
-
-            Glide.with(itemView.getContext())
-                    .load(chienDich.getHinhAnh())
-                    .into(imgchiendichUngHo);
-
-            if (chienDich.getSoTienMucTieu().doubleValue() > 0) {
-                int progress = chienDich.getSoTienHienTai().multiply(new java.math.BigDecimal(100))
-                        .divide(chienDich.getSoTienMucTieu(), RoundingMode.HALF_UP).intValue();
-                progressBarItem.setProgress(progress);
-            } else {
-                progressBarItem.setProgress(0);
-            }
-
-            itemView.setOnClickListener(v -> listener.onChienDichClick(chienDich));
         }
     }
 }
