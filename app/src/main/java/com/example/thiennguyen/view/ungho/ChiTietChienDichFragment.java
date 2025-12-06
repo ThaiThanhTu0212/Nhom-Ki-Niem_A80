d1e9eede5ecc3d3b4f1012a61ff110c765de723e
TuanAnhNguyen-2005 <Tn271574@gmail.com>
2025-12-06 23:32:01 +0700

    commit1

--- app/src/main/java/com/example/thiennguyen/view/ungho/ChiTietChienDichFragment.java
+++ app/src/main/java/com/example/thiennguyen/view/ungho/ChiTietChienDichFragment.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +19,8 @@ import com.example.thiennguyen.R;
 import com.example.thiennguyen.view.model.ChienDich;
 
 import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 public class ChiTietChienDichFragment extends Fragment {
@@ -52,7 +55,10 @@ public class ChiTietChienDichFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_chi_tiet_chien_dich, container, false);
 
         if (chienDich != null) {
-            ImageView imgChienDich = view.findViewById(R.id.imgChienDich);
+            RecyclerView imageRecyclerView = view.findViewById(R.id.imageRecyclerView);
+            ImageAdapter imageAdapter = new ImageAdapter(getContext(), getSampleImageUrls());
+            imageRecyclerView.setAdapter(imageAdapter);
+
             TextView tvTenChienDich = view.findViewById(R.id.tvTenChienDich);
             TextView tvTenToChuc = view.findViewById(R.id.tvTenToChuc);
             TextView tvMoTa = view.findViewById(R.id.tvMoTa);
@@ -61,7 +67,6 @@ public class ChiTietChienDichFragment extends Fragment {
             TextView tvSoTienMucTieu = view.findViewById(R.id.tvSoTienMucTieu);
             Button btnUngHo = view.findViewById(R.id.btnUngHo);
 
-            Glide.with(this).load(chienDich.getHinhAnh()).into(imgChienDich);
             tvTenChienDich.setText(chienDich.getTenCd());
             tvTenToChuc.setText(chienDich.getNguoiToChuc().getHoTen());
             tvMoTa.setText(chienDich.getMoTa());
@@ -79,6 +84,16 @@ public class ChiTietChienDichFragment extends Fragment {
         }
 
         return view;
+    }
+
+    private List<String> getSampleImageUrls() {
+        return Arrays.asList(
+                "https://i.pinimg.com/564x/b8/69/27/b8692797a22459434bedb93301077334.jpg",
+                "https://i.pinimg.com/564x/4b/7e/37/4b7e373b8a2e58a23351945a8f579d4b.jpg",
+                "https://i.pinimg.com/564x/a2/45/63/a24563a61f3f615f3f4e2f3d61172c1c.jpg",
+                "https://i.pinimg.com/564x/d1/a7/6b/d1a76b0f1d82e2e7a4f89a9f2d15024b.jpg",
+                "https://i.pinimg.com/564x/e7/7c/49/e77c49b145e1b1d1f7c3c3a9f73315a2.jpg"
+        );
     }
 
     private void showQrDialog() {
