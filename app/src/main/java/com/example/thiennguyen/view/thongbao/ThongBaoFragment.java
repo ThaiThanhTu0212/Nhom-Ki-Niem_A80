1795b4766f741a5aeeb2d513d38170261f81bd0f
TuanAnhNguyen-2005 <Tn271574@gmail.com>
2025-12-06 23:34:04 +0700

    commit1

--- app/src/main/java/com/example/thiennguyen/view/thongbao/ThongBaoFragment.java
+++ app/src/main/java/com/example/thiennguyen/view/thongbao/ThongBaoFragment.java
@@ -33,7 +33,7 @@ public class ThongBaoFragment extends Fragment implements ThongBaoAdapter.OnThon
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_thong_bao_layout, container, false);
+        View view = inflater.inflate(R.layout.layout_thong_bao, container, false);
 
         ImageView imgClose = view.findViewById(R.id.imgClose);
         imgClose.setOnClickListener(v -> getParentFragmentManager().popBackStack());
